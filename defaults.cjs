e252e436c57defb49680e900507fb0968a08e344
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-02-17 14:11:12 +0530

    Add default file with default method
    This will set the default values in to the object if the object does not have that key

---
+++ defaults.cjs
@@ -0,0 +1,19 @@
+
+function defaults(testObject,defaultProps){  
+   
+    if (!testObject || !(typeof testObject === 'object')){
+        return [];
+    }
+    else{
+
+        for(let defaultPropsKey in defaultProps){
+            
+            if ((defaultPropsKey in testObject === false)){
+            
+                testObject[defaultPropsKey] = defaultProps[defaultPropsKey];
+            }
+        }
+        return testObject;   
+    }
+}
+module.exports = defaults;
